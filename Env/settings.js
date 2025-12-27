84097c4838dcd4e112cfac6038967fa6fcc0122e
vertigo <vertigo@example.com>
2025-12-27 10:08:42 +0000

    getcmd.js

--- Env/settings.js
+++ Env/settings.js
@@ -3,7 +3,7 @@ const mycode = process.env.CODE || "254799431732";
 const botname = process.env.BOTNAME || '🄵🄴🄴-🅇🄼🄳';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
-const database = process.env.DATABASE_URL || '';
+const database = process.env.DATABASE_URL || ''; 
 
 module.exports = {
   session,
